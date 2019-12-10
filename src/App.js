aabadddef2bcd23f6de520455d0ecad761744846
Miley Wright <dishaliekwright@yahoo.com>
2019-12-09 23:08:09 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Mileys Project
         </a>
       </header>
     </div>
